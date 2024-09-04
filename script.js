102f157653081d2bbccbbb2f1d69ba284f120398
Ayub <ayubola@gmail.com>
2024-09-04 15:55:52 -0700

    frontend

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 let number = prompt("Please enter a number:");
 number = Number(number);
 
-if (number > 0) {
-    alert(1);
-} else if (number < 0) {
-    alert(-1);
-} else {
+if (number > 1) {
     alert(0);
+} else if (number < 0) {
+    alert(0);
+} else {
+    alert(-1);
 }
 
 
